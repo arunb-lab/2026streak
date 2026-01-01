e83c204587212cb857c54b6194882491d5a3bdaf
Aroon <baralbhuwan6@gmail.com>
2026-01-01 20:43:27 +0545

    New year #100days of pyhton

---
+++ variable.py
@@ -0,0 +1,17 @@
+#how do you d eclare variables inpython ?
+# In python, You just write the varibale on the left,followed by assignment operator, and the value  you want to assign the variable on the right.
+
+#Example ::
+name = "John Doe"  # string variable
+age = 20 # integer variable
+height = 5.9  # float variable
+
+#when naming variables in python, you should follow these rules:
+# 1: Varibale name can only start with a letter or an undescore_ but not a number.
+# 2: Variable name can oly contain Number alphabet and underscores
+# 3: Variable names are case sensetive.
+# 4: Python's reserved keywords cannont be use as a variable name. example : print, if, class, def
+# 5: You should use descriptive names for variables. 
+# Example:
+ user_age = 25
+ user_name = "Arun Baral"
