26c2fb1ba673bddf2201d9d29a2770e5fe3670ef
Aroon <baralbhuwan6@gmail.com>
2026-01-05 21:28:14 +0545

    day 4 fibonacci series

--- nonkeywordargs.py
+++ nonkeywordargs.py
@@ -3,3 +3,11 @@ def myFun(*argv):
         print(arg)
 
 myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
+
+def multiply(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+print(multiply(2, 3, 4))
