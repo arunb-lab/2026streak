aa77c45c42f2f9ba382aab1fa1e502bd1599ab3c
Aroon <baralbhuwan6@gmail.com>
2026-01-19 21:33:58 +0545

    looplist

---
+++ pizzaloop.py
@@ -0,0 +1,7 @@
+pizzas = ['margherita','pepperoni','cheese','veggie','hawaiian']
+
+for pizza in pizzas :
+    print("I like " + pizza.title() + " Pizza !")
+print()
+print("I really Like " + pizzas[0].title() + " Pizza")
+
