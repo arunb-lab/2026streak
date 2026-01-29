45a2a3b96550a200b7647b857a77798b6cb92e99
Aroon <baralbhuwan6@gmail.com>
2026-01-29 22:47:22 +0545

    Learning Python

---
+++ if-statement/task.py
@@ -0,0 +1,9 @@
+username = ['admin', 'arun', 'john', 'sara', 'linda']
+if username:
+    for user in username:
+        if user == 'admin':
+            print("Hii, " + user +" Would you like to see a status report ??" )
+        else:
+            print("Hii, " + user + "! Thanks for logging in again.")
+print()
+print("Thank you for using our software! ")
